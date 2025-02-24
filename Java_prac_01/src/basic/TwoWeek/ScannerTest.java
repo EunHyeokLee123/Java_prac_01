41ae94ca96822395fe26f160a0b7419b3fb3342b
EunHyeok <secun77@naver.com>
2025-02-24 12:14:29 +0900

    스캐너 공부

---
+++ Java_prac_01/src/basic/TwoWeek/ScannerTest.java
@@ -0,0 +1,11 @@
+package basic.TwoWeek;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
