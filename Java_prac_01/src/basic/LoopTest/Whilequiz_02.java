d7ad79e2e16c2525025c1e02a0e5576fa1563b0b
EunHyeok <secun77@naver.com>
2025-02-25 10:31:31 +0900

    이름 및 숫자 변경

--- Java_prac_01/src/basic/LoopTest/Whilequiz_02.java
+++ Java_prac_01/src/basic/LoopTest/Whilequiz_02.java
@@ -2,23 +2,27 @@ package basic.LoopTest;
 
 import java.util.Scanner;
 
-public class Whilequiz_02 {
+public class WhileQuiz_02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("정수를 하나 입력하세요. ");
-        int tar = sc.nextInt();
-        boolean result = true;
-        for(int i = 2; i < tar; i++){
-            if(tar % i == 0){
-                result = false;
-            }
+        int a1, a2;
+        System.out.println("정수 1을 입력하시오.");
+        a1 = sc.nextInt();
+        System.out.println("정수 2를 입력하시오.");
+        a2 = sc.nextInt();
+        /*if(a1>a2){
+            int tmp = a2;
+            a2 = a1;
+            a1 = tmp;
+        }*/
+        int start = a1 > a2 ? a2 : a1;
+        int end = a2 > a1 ? a2: a1;
+        int idx = a1; int result = 0;
+        while(idx <= a2){
+            result += idx;
+            idx++;
         }
-        if(result){
-            System.out.printf("%d는 소수입니다.", tar);
-        }
-        else {
-            System.out.printf("%d는 소수가 아닙니다.", tar);
-        }
+        System.out.printf("%d부터 %d까지의 누적 합계는 %d입니다. \n", a1, a2, result);
 
 
 
--- Java_prac_01/src/basic/constate/IfQuiz_02.java
+++ Java_prac_01/src/basic/constate/IfQuiz_02.java
@@ -1,9 +1,9 @@
-package basic.LoopTest;
+package basic.constate;
 
 import java.util.Scanner;
 
 
-public class WhileQuiz_01 {
+public class IfQuiz_02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a1, a2, a3;
