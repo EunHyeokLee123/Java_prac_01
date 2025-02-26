3e2caf423484915f438777694b49f7d1932c7825
EunHyeok <secun77@naver.com>
2025-02-26 16:35:12 +0900

    JAVA_DAY_04 객체의 생성자 중복 선언 공부

---
+++ Java_prac_01/src/oop/Constructor/Main.java
@@ -0,0 +1,19 @@
+package oop.Constructor;
+
+public class Main {
+    public static void main(String[] args) {
+        Phone ph1 = new Phone();
+        ph1.color = "Red";
+        ph1.model = "IPhone 14 plus";
+        ph1.price = 1350000;
+        ph1.showSpec();
+        Phone ph2 = new Phone();
+        ph2.color = "Midnight Black";
+        ph2.model = "IPhone 13";
+        ph2.price = 1250000;
+        Phone ph3 =  new Phone("Galaxy s25", "다라이블루", 990000);
+        ph3.showSpec();
+        Phone ph4 = new Phone();
+        ph4.showSpec();
+    }
+}
