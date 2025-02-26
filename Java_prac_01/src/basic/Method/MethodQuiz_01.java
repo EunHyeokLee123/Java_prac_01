18967c6128fee8e8e4ed30ba0fd80a43c3f61115
EunHyeokLee123 <129397987+EunHyeokLee123@users.noreply.github.com>
2025-02-26 20:07:28 +0900

    Update MethodQuiz_01.java

--- Java_prac_01/src/basic/Method/MethodQuiz_01.java
+++ Java_prac_01/src/basic/Method/MethodQuiz_01.java
@@ -22,12 +22,107 @@ public class MethodQuiz_01 {
         return result;
     }
 
+// 특정 요소가 배열에 포함되어 있는지의 여부를 확인하는 메서드를 작성하세요.
+    static boolean isInclude(String searchTarget) {
+        boolean result = false;
+        for (String ab : foods) {
+            if(searchTarget.equals(ab)){
+                result = true;
+                break;
+            }
+        }
+        return  result;
+    }
 
+
+
+    // foods 배열에 데이터를 끝에 추가하는 메서드를 작성하세요.
+    static void push(String newFood) {
+        String[] temp = new String[foods.length + 1];
+        for (int i = 0; i < foods.length; i++) {
+            temp[i] = foods[i];
+        }
+        temp[temp.length - 1] = newFood;
+        foods = temp; temp = null;
+    }
+
+
+    // 특정 인덱스가 범위 내의 올바른 인덱스인지를 확인하는 메서드를 작성하세요.
+    // 작성이 완료되면 타 메서드에서 활용할 수 있을 것입니다.
+    static boolean isOutOfBounds(int targetIndex) {
+        if(targetIndex > foods.length){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+
+    // 원하는 인덱스 위치에 요소를 삽입하는 메서드를 작성하세요.
+    // targetIndex는 기존 foods의 배열 인덱스 범위 내여야 합니다.
+    static void insert(int targetIndex, String newFoodName) {
+        if(!isOutOfBounds(targetIndex)){
+            System.out.println("유효하지 않은 인덱스입니다.");
+            return;
+        }
+        String[] temp = new String[foods.length + 1];
+        for (int i = 0; i < targetIndex; i++) {
+            temp[i] = foods[i];
+        }
+        temp[targetIndex] = newFoodName;
+        for (int i = targetIndex + 1; i < temp.length; i++) {
+            temp[i] = foods[i-1];
+        }
+        foods = temp; temp = null;
+    }
+
+
+    // 원하는 인덱스 위치의 요소를 수정하는 메서드를 작성하세요.
+    // targetIndex는 기존 foods의 배열 인덱스 범위 내여야 합니다.
+    static void modify(int targetIndex, String newFoodName) {
+        if(!isOutOfBounds(targetIndex)){
+            System.out.println("유효하지 않은 인덱스입니다.");
+            return;
+        }
+        foods[targetIndex] = newFoodName;
+    }
+
+
+    // 특정 인덱스의 요소를 삭제하는 메서드를 작성하세요.
+    static void remove(String deleteTarget) {
+        if(!isInclude(deleteTarget)){
+            System.out.println(deleteTarget+"은 배열에 없습니다.");
+            return;
+        }
+        int idx = indexOf(deleteTarget);
+        foods[idx] = null;
+        for (int i = idx; i < foods.length - 1; i++) {
+            String tmp = foods[i];
+            foods[i] = foods[i+1];
+            foods[i+1] = tmp;
+        }
+        String[] temp = new String[foods.length - 1];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = foods[i];
+        }
+        foods = temp; temp = null;
+    }
+
+
+    // 배열 내부를 깔끔하게 비워버리는 메서드를 작성하세요. (삭제 반복 x)
+    static void clear(){
+        for (int i = 0; i < foods.length; i++) {
+            foods[i] = null;
+        }
+    }
 
     public static void main(String[] args) {
         printfoods(foods);
         int a1 = indexOf("pork");
         System.out.println("a1 = " + a1);
+        remove("beef");
+        System.out.println(Arrays.toString(foods));
     }
 
 }
