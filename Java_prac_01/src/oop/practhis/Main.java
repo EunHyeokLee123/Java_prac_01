b315a80c36936c39977534eedb141ae9ed352f74
EunHyeok <secun77@naver.com>
2025-02-27 12:05:38 +0900

    JAVA_DAY_05 this 및 생성자 공부

--- Java_prac_01/src/oop/practhis/Main.java
+++ Java_prac_01/src/oop/practhis/Main.java
@@ -5,10 +5,10 @@ public class Main {
         Player p1 = new Player();
         p1.nickName = "James";
         Player p2 = new Player("Hamel");
+        p1.attack(p1);
+        p1.attack(p2);
+        Player p3 = new Player("Ronaldo", 44);
         System.out.println(p1.toString());
         System.out.println(p2.toString());
-        Player p3 = new Player();
-        p3.nickName = "James";
-        Player p4 = new Player("Hamel");
     }
 }
--- Java_prac_01/src/oop/practhis/Player.java
+++ Java_prac_01/src/oop/practhis/Player.java
@@ -6,32 +6,52 @@ public class Player {
     int level;
     int hp;
 
-    // alt + insert
-    /*@Override
-    public String toString() {
-        return "Player{" +
-                "nickName='" + nickName + '\'' +
-                ", level=" + level +
-                ", hp=" + hp +
-                '}';
-    }*/
 
-    // this는 현재 호출한 객체의 주소를 의미함.
     public Player() {
-        System.out.println("플레이어의 기본" +
-                " 생성자가 호출됨.");
-        System.out.println("this = " + this);
+        // this는 현재 호출한 객체의 주소를 의미함.
+        // System.out.println("this = " + this);
         level = 1;
         hp = 50;
     }
 
     // this가 붙으면 이 객체의 필드 속 정보로 특정 짓게함.
     public Player(String nickName) {
+        this();
         this.nickName = nickName;
-        System.out.println("플레이어의 이름이 " +
-              nickName + "인 생성자가 호출됨.");
-        System.out.println("this = " + this);
-        level = 1;
-        hp = 50;
     }
+
+    Player(String nickName, int hp){
+        this(nickName);
+        this.hp = hp;
+    }
+
+    void attack(Player target){
+        if (target != this) {
+            target.hp -= 10;
+            System.out.printf(this.nickName + "이 " + target.nickName +
+                    "을 공격하여 10의 데미지를 입혔습니다. \n");
+            if(target.hp < 0){
+                System.out.printf(target.nickName + " 님이 사망하였습니다. \n");
+                System.out.println("다시 부활합니다.");
+            }
+            else{
+                System.out.printf(target.nickName+" 님의 남은 체력은 "+
+                        target.hp+"입니다.\n");
+            }
+        }
+        else {
+            System.out.println("자기 자신은 공격할 수 없습니다.");
+        }
+    }
+
+    // alt + insert
+    @Override
+    public String toString() {
+        return "Player{" +
+                "nickName='" + nickName + '\'' +
+                ", level=" + level +
+                ", hp=" + hp +
+                '}';
+    }
+
 }
