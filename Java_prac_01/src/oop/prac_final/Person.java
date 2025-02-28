e40a1d4111c419a8e9090ee6304eeb9e7d18da43
EunHyeok <secun77@naver.com>
2025-02-28 10:28:13 +0900

    JAVA_DAY_06 final 키워드 공부

---
+++ Java_prac_01/src/oop/prac_final/Person.java
@@ -0,0 +1,17 @@
+package oop.prac_final;
+
+public class Person {
+
+    // 초기화되지 않은 변수는 final 불가능
+    final String name;
+    final String nation;
+    int age;
+
+    // 생성 시 name 초기화가 필수로 일어나서
+    // name에 final을 붙일 수 있다.
+    Person(String name, String nation){
+        this.name = name;
+        this.nation = nation;
+    }
+
+}
