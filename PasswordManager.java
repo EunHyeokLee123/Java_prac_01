ac6a7a8f064edc517bb2a7e5a1c8b1c7296bee4f
EunHyeokLee123 <129397987+EunHyeokLee123@users.noreply.github.com>
2025-03-03 22:04:59 +0900

    클래스 퀴즈

---
+++ PasswordManager.java
@@ -0,0 +1,17 @@
+package oop.Quiz_class;
+
+public class PasswordManager {
+    private String password;
+
+    PasswordManager(String password){
+        this.password = password;
+    }
+
+    public boolean changePassword(String oldpw, String newpw){
+        if(oldpw.equals(this.password)){
+            this.password = newpw;
+            return true;
+        }else
+            return false;
+    }
+}
