255b64490443e0adc573f442b4d430b57d49a5a7
EunHyeokLee123 <secun77@naver.com>
2025-03-11 20:59:54 +0900

    25206 해결

--- Java_prac_01/src/CodeStudy/No_25206.java
+++ Java_prac_01/src/CodeStudy/No_25206.java
@@ -3,39 +3,43 @@ package CodeStudy;
 import java.util.Scanner;
 
 public class No_25206 {
+
+    static double whatScore(String str){
+        if(str.equals("A+"))
+            return 4.5;
+        else if (str.equals("A0"))
+            return 4.0;
+        else if (str.equals("B+"))
+            return 3.5;
+        else if (str.equals("B0"))
+            return 3.0;
+        else if (str.equals("C+"))
+            return 2.5;
+        else if (str.equals("C0"))
+            return 2.0;
+        else if (str.equals("D+"))
+            return 1.5;
+        else if (str.equals("D0"))
+            return 1.0;
+        else if (str.equals("F"))
+            return 0.0;
+        else
+            return  5.0;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] str = new String[20];
-        for (int i = 0; i < str.length; i++) {
-            String tmp = sc.next();
-            tmp = tmp.substring(tmp.length()-2);
-            tmp = tmp.trim();
-            str[i] = tmp;
+        double re = 0.0; double time = 0.0;
+        for (int i = 0; i < 20; i++) {
+            String ins = sc.nextLine();
+            String[] strs = ins.split(" ");
+            double tmp = whatScore(strs[2]);
+            if(tmp != 5.0){
+                re += Double.parseDouble(strs[1]) * tmp;
+                time += Double.parseDouble(strs[1]);
+            }
         }
-        double re = 0.0; int count = 20;
-        for (String s : str) {
-            if(s.equals("A+"))
-                re += 4.5;
-            else if(s.equals("A0"))
-                re += 4.0;
-            else if (s.equals("B+"))
-                re+=3.5;
-            else if (s.equals("B0"))
-                re+=3.0;
-            else if (s.equals("C+"))
-                re+=2.5;
-            else if (s.equals("C0"))
-                re+=2.0;
-            else if (s.equals("D+"))
-                re+=1.5;
-            else if (s.equals("D0"))
-                re+=1.0;
-            else if (s.equals("F"))
-                re+=0.0;
-            else if (s.equals("P"))
-                count--;
-        }
-        re = re/count;
+        re /= time;
         System.out.println(re);
         sc.close();
     }
