ec63700fd01bea33197f2e0f307666d2aaea4c2e
EunHyeok <secun77@naver.com>
2025-04-07 17:33:54 +0900

    7562 해결중

--- Java_prac_01/src/CodeStudy/solution_7562/Main.java
+++ Java_prac_01/src/CodeStudy/solution_7562/Main.java
@@ -3,6 +3,7 @@ package CodeStudy.solution_7562;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,23 +14,42 @@ public class Main {
     static int[] dy = {2, 1, -1, -2, -2, -1, 1, 2};
 
     static int times = 0;
+    static boolean find = false;
+    static ArrayDeque<int[]> road;
+
+    public static boolean canGo(int[] now, int[] des, int len){
+        for (int i = 0; i < 8; i++) {
+            int x = now[0] + dx[i];
+            int y = now[1] + dy[i];
+            if(x >= 0 && x< len && y >= 0 && y < len){
+                if(x == des[0] && y == des[1]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 
     public static void BFS(int[] start, int[] des, int len){
-        int[] now = start;
+        if (start != des) {
+            road.offer(start);
+        }
+        else{
+            return;
+        }
         while(true){
-            if(now == des){
-                return;
-            }
+            int[] now = road.poll();
             for (int i = 0; i < 8; i++) {
                 int x = now[0] + dx[i];
                 int y = now[1] + dy[i];
-                times++;
-                if(x >= 0 && x < len && y >= 0 && y < len){
+                if(x >= 0 && x< len && y >= 0 && y < len){
                     if(x == des[0] && y == des[1]){
                         return;
                     }
                     else{
-
+                        int[] tmp = {x, y};
+                        road.offer(tmp);
                     }
                 }
             }
@@ -42,6 +62,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(br.readLine());
         result = new int[count];
+        road = new ArrayDeque<>();
         for (int i = 0; i < count; i++) {
             int len = Integer.parseInt(br.readLine());
             StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
