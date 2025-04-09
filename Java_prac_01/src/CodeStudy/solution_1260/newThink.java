2f0cad03e12dd4cdb55da9ef11b791b05b8d897d
EunHyeok <secun77@naver.com>
2025-04-09 13:33:45 +0900

    1260, 2667 주석달음

--- Java_prac_01/src/CodeStudy/solution_1260/newThink.java
+++ Java_prac_01/src/CodeStudy/solution_1260/newThink.java
@@ -10,39 +10,66 @@ public class newThink {
     static int[][] path;
     static int point;
     static int start;
+    // 경로 탐색 여부
     static boolean[] check;
+    // DFS용 스택
     static Stack<Integer> Dre;
+    // BFS용 큐
     static Queue<Integer> Bre;
+    // DFS 경로 저장용
     static ArrayList<Integer> re1;
+    // BFS 경로 저장용
     static ArrayList<Integer> re2;
 
     public static void DFS(int now){
+        // 매개값 일단 스택에 삽입
         Dre.push(now);
+        // 방문 처리
         check[now] = true;
+        // 결과 리스트에 삽입
         re1.add(now);
         for (int i = 1; i <= point; i++) {
+            // 방문하지 않은 지점 중 갈 수 있는 지점 확인
             if(path[now][i] == 1 && !check[i]){
+                // 해당 지점에 대해서 DFS 재귀 호출
                 DFS(i);
+                // 해당 지점 탐색 후 방문 여부를 다시 초기화하지 않음
+                // 그래야 처음 탐색한 경로만 출력할 수 있음.
             }
         }
+        // 해당 지점 탐색 끝났으면 pop함
         Dre.pop();
+        // 스택이 비었으면 -> 탐색이 끝남.
         if(Dre.isEmpty()) return;
     }
 
     public static void BFS(){
+        // 시작점 방문 처리
         check[start] = true;
+        // 결과 리스트에 삽입
         re2.add(start);
+        // 현재 방문 지점을 시작점으로 초기화
         int tmp = start;
         while(true){
+            // 해당 지점에서 갈 수 있는 모든 지점을 탐색
             for (int i = 1; i <= point; i++) {
+                // 유효한 지점인지 확인
                 if(path[tmp][i] == 1 && !check[i]){
-                    Bre.offer(i); check[i] = true;
+                    // 갈 수 있는 모든 지점을 탐색 순서에 맞게 enque
+                    Bre.offer(i); 
+                    // 해당 지점 방문 처리
+                    check[i] = true;
+                    // 결과 리스트에 삽입
                     re2.add(i);
                 }
+                // 방문 여부를 초기화하지 않아서
+                // 초기 탐색 경로만 출력됨.
             }
+            // 큐가 비어있다면 모든 지점을 탐색한 거라서 탐색 종료
             if(Bre.isEmpty()){
                 return;
             }else{
+                // 큐가 비어있지 않다면 deque해서 현재 방문 지점 갱신
                 tmp = Bre.poll();
             }
         }
@@ -67,15 +94,19 @@ public class newThink {
             path[a1][a2] = 1;
             path[a2][a1] = 1;
         }
+        // static 요소 초기화
         Dre = new Stack<>();
         Bre = new ArrayDeque<>();
         re1 = new ArrayList<>();
         re2 = new ArrayList<>();
-
+    
+        // DFS 실행
         DFS(start);
         for (int i = 0; i <= point; i++) {
+            // 방문 여부 배열이 static이라 초기화
             check[i] = false;
         }
+        // BFS 실행
         BFS();
 
         for (Integer i : re1) {
--- Java_prac_01/src/CodeStudy/solution_2667/Main.java
+++ Java_prac_01/src/CodeStudy/solution_2667/Main.java
@@ -8,23 +8,31 @@ import java.util.Collections;
 import java.util.LinkedList;
 
 public class Main {
-
+    
+    // 지도의 크기
     static int len;
+    // 입력값 받는 리스트
     static ArrayList<ArrayList<Integer>> arr;
+    // 영역 개수
     static int count = 0;
+    // 영역별 집의 개수 저장용
     static LinkedList<Integer> resultlist = new LinkedList<>();
+    // 영역별 집의 개수를 세는 용도
     static int temp = 0;
+    // 상하좌우 탐색을 위한 x, y값 배열 2개
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
-
+    // 집(1인 값)을 찾기 위한 메소드
     public static int[] findPa(){
+        // 만약 집이 더이상 없으면 [-1, -1] 리턴
         int[] result = {-1, -1};
         for (int i = 0; i < arr.size(); i++) {
             for (int j = 0; j < arr.get(i).size(); j++) {
                 if(arr.get(i).get(j) == 1){
                     result[0] = i;
                     result[1] = j;
+                    return result;
                 }
             }
         }
@@ -35,12 +43,17 @@ public class Main {
     public static void DFS(int x, int y){
         // 방문 처리
         arr.get(x).set(y, -1);
+        // 집의 개수 갱신
         temp++;
         for (int i = 0; i < dx.length; i++) {
+            // 상하좌우 좌표값 계산
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx >=0 && ny >=0 && nx <len && ny < len){
+            // 좌표값의 유효성 확인
+            if(nx >=0 && ny >=0 && nx < len && ny < len){
+                // 집을 찾으면
                 if(arr.get(nx).get(ny) == 1)
+                    // 그 집에서 다시 DFS 함수 호출
                     DFS(nx, ny);
             }
         }
@@ -64,15 +77,22 @@ public class Main {
             }
         }
         while(true){
+            // 먼저 집을 하나 찾음
             int[] tmp = findPa();
+            // 집이 더이상 없으면 break
             if(tmp[0] == -1){
                 break;
             }
+            // 번지의 집의 개수를 초기화 (static으로 계속 쓸거라서)
             temp = 0;
+            // DFS 실행
             DFS(tmp[0], tmp[1]);
+            // 종료되면 결과리스트에 집의 개수 삽입
             resultlist.add(temp);
+            // 단지 개수 갱신
             count++;
         }
+        // 오름차순 정렬
         Collections.sort(resultlist);
         System.out.println(count);
         for (Integer i : resultlist) {
