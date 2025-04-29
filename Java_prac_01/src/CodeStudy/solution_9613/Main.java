0f9f666e34c62005fadc73d5c2b987e62210d48b
EunHyeok <secun77@naver.com>
2025-04-29 13:35:18 +0900

    9613 해결!

--- Java_prac_01/src/CodeStudy/solution_9613/Main.java
+++ Java_prac_01/src/CodeStudy/solution_9613/Main.java
@@ -8,41 +8,46 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int whatGCD(int a, int b) {
-        int max, min = 0;
-        if(a <= b){
-            max =  b;
-            min = a;
+    static ArrayList<Long> list = new ArrayList<>();
+
+    // 유클리드 호제법!!
+    static int whatGCD(int a1, int a2) {
+        int a, b = 0;
+        if (a1 > a2) {
+            a = a1;
+            b = a2;
         }
         else{
-            max = a;
-            min = b;
+            a = a2;
+            b = a1;
         }
-        for (int i = 1; i < (int) Math.sqrt(min); i++) {
-            if(min % i == 0){
-                int temp = min / i;
-                if(max % temp == 0){
-                    return temp;
-                }
-            }
+        while(b != 0){
+            int temp = a % b;
+            a = b;
+            b = temp;
         }
-        return 1;
+        return a;
     }
 
-    static int calcGCD(int[] arr, int len){
-        int result = 0;
+    static long calcGCD(int[] arr, int len){
+        long result = 0;
         int left = 0; int right = 1;
         while(left < len - 1) {
             while(right < len){
-
+                int mid = whatGCD(arr[left], arr[right]);
+                result += mid;
+                right++;
+                //System.out.println(mid);
             }
+            left++;
+            right = left + 1;
         }
+        return result;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(br.readLine());
-        ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int len = Integer.parseInt(st.nextToken());
@@ -50,9 +55,10 @@ public class Main {
             for (int j = 0; j < len; j++) {
                 arr[j] = Integer.parseInt(st.nextToken());
             }
-
-
-
+            list.add(calcGCD(arr, len));
+        }
+        for (Long i : list) {
+            System.out.println(i);
         }
 
 
