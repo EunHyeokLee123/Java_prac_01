2219667f40cff091a95031ba3b42a4bfed693c88
EunHyeok <secun77@naver.com>
2025-05-26 17:30:20 +0900

    1509는 다시 꼭 봐야됨....

--- Java_prac_01/src/CodeStudy/solution_1509/Main.java
+++ Java_prac_01/src/CodeStudy/solution_1509/Main.java
@@ -6,37 +6,45 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static boolean isFal(String str, int start, int end){
-        while(start < end) {
-            if(str.charAt(start) != str.charAt(end)) {
-                return false;
-            }
-            start++; end--;
-        }
-        return true;
-    }
+
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        boolean[][] checked = new boolean[input.length()][input.length()];
-        for (int i = 0; i < input.length(); i++) {
-            checked[i][i] = true;
-        }
-        for (int i = 0; i < input.length(); i++) {
-            for (int j = i + 1; j < input.length(); j++) {
-                if(isFal(input, i, j)) {
-                    if(!checked[i][j]) {
-                        checked[i][j] = true;
+        boolean[][] checked = new boolean[input.length() + 1][input.length() + 1];
+
+        // 모든 팰린드롬 구하기
+        for (int i = 1; i <= input.length(); i++) {
+            for (int j = i; j <= input.length(); j++) {
+                boolean flag = true;
+                int s = i - 1; int e = j - 1;
+                while(s <= e){
+                    if(input.charAt(s) != input.charAt(e)){
+                        flag = false;
+                        break;
                     }
+                    s++; e--;
+                }
+                if(flag){
+                    checked[i][j] = true;
+                }
+            }
+        }
+        int[] dp = new int[input.length() + 1];
+        for (int i = 1; i <= input.length(); i++) {
+            dp[i] = input.length();
+        }
+        for (int i = 1; i <= input.length(); i++) {
+            for (int j = 1; j <= i; j++) {
+                if(checked[j][i]){
+                    dp[i] = Math.min(dp[i], dp[j-1] + 1);
                 }
             }
         }
 
-
-
-        /*
+        System.out.println(dp[input.length()]);
+/*
         for (int i = 0; i < input.length(); i++) {
             for (int j = 0; j < input.length(); j++) {
                 if(checked[i][j]){
@@ -47,8 +55,7 @@ public class Main {
                 }
             }
         }
-        */
-
+      */
         br.close();
     }
 
