1b9fa47c463bc5dfcde8ef2d72b902b6e5bd7f68
EunHyeokLee123 <secun77@naver.com>
2025-06-09 22:46:45 +0900

    1720 해결. 이해는 잘 안됨

--- Java_prac_01/src/CodeStudy/solution_1720/Main.java
+++ Java_prac_01/src/CodeStudy/solution_1720/Main.java
@@ -3,8 +3,6 @@ package CodeStudy.solution_1720;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
@@ -14,12 +12,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int len = Integer.parseInt(br.readLine());
 
+        // 전체 경우
+        int[] dp1 = new int[31];
 
+        // 펠린드롬의 경우, 좌우대칭
+        int[] dp2 = new int[31];
 
+        dp1[0] = 0; dp1[1] = 1; dp1[2] = 3; dp1[3] = 5;
 
+        dp2[0] = 1; dp2[1] = 1; dp2[2] = 3; dp2[3] = 1;
 
+        for (int i = 4; i <= len; i++) {
+            dp1[i] = dp1[i - 1] + (dp1[i-2] * 2);
+            dp2[i] = dp2[i - 2] + (dp2[i - 4] * 2);
+        }
 
+        int result = (dp1[len] - dp2[len])/2 + dp2[len];
 
+        System.out.println(result);
         br.close();
     }
 
