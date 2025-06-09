b39f5d79abf680bdeefb32c089ee75abd59277de
EunHyeok <secun77@naver.com>
2025-06-09 22:10:38 +0900

    1720 해결중

---
+++ Java_prac_01/src/CodeStudy/solution_1720/Main.java
@@ -0,0 +1,26 @@
+package CodeStudy.solution_1720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int len = Integer.parseInt(br.readLine());
+
+
+
+
+
+
+
+        br.close();
+    }
+
+}
