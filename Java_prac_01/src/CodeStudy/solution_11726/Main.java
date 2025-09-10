4896661f43e9209a26bbfef21d5dec0dd8e95305
EunHyeokLee123 <secun77@naver.com>
2025-09-10 21:51:21 +0900

    11726 미해결

---
+++ Java_prac_01/src/CodeStudy/solution_11726/Main.java
@@ -0,0 +1,24 @@
+package CodeStudy.solution_11726;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(br.readLine());
+
+
+
+
+
+
+
+
+        br.close();
+    }
+
+}
