cc86e98ed4469450978df93ec316ad7b51d9e13f
EunHyeok <secun77@naver.com>
2025-09-14 17:17:57 +0900

    11054 감소하는 부분을 구하는 로직의 문제점 해결

--- Java_prac_01/src/CodeStudy/solution_11054/Main.java
+++ Java_prac_01/src/CodeStudy/solution_11054/Main.java
@@ -3,6 +3,7 @@ package CodeStudy.solution_11054;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,6 +16,11 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < length; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+        }
+        // 감소하는 부분을 증가하는 부분을 구하는 로직과 동일하게 사용하기 위해 역순 배열을 만듦.
+        int[] reverse = new int[length];
+        for (int i = 0; i < length; i++) {
+            reverse[i] = arr[length - 1 - i];
         }
         // 커지는 것
         int[] upper = new int[length];
@@ -29,19 +35,22 @@ public class Main {
                 if(arr[i] > arr[j]) {
                     upper[i] = Math.max(upper[i], upper[j] + 1);
                 }
-                else if (arr[i] < arr[j]) {
+            }
+            for (int j = 0; j < i; j++) {
+                if(reverse[i] > reverse[j]) {
                     lower[i] = Math.max(lower[i], lower[j] + 1);
                 }
             }
         }
         int[] bitonic = new int[length];
         for (int i = 0; i < length; i++) {
-            bitonic[i] = upper[i] + lower[i] - 1;
+            // lower의 3번째 값은 실제로는 length - 1 - 3의 값이 맞음.
+            bitonic[i] = upper[i] + lower[length - 1 - i] - 1;
         }
 
         int max = 1;
         for (int i = 0; i < length; i++) {
-            int temp = Math.max(upper[i], Math.max(lower[i], bitonic[i]));
+            int temp = Math.max(upper[i], Math.max(lower[length - 1 - i], bitonic[i]));
             if(temp > max) {
                 max = temp;
             }
