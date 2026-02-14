1383f16379352636ea06d76b8c86870d09181ef8
EunHyeok <secun77@naver.com>
2026-02-14 16:38:02 +0900

    17144 해결중

--- Java_prac_01/src/CodeStudy/solution_17144/Main.java
+++ Java_prac_01/src/CodeStudy/solution_17144/Main.java
@@ -25,7 +25,7 @@ public class Main {
                 if(arr[i][j] > 0) {
                     int count = 0;
                     for(int k = 0; k < 4; k++) {
-                        int ny = i + dy[i];
+                        int ny = i + dy[k];
                         int nx = j + dx[k];
                         if(ny >= 0 && ny < R && nx >= 0 && nx < C) {
                             if(arr[ny][nx] != -1) {
@@ -40,7 +40,7 @@ public class Main {
                     arr[i][j] = remain;
                     // 새로 사용할 배열에 추가될 값을 저장
                     for(int k = 0; k < 4; k++) {
-                        int ny = i + dy[i];
+                        int ny = i + dy[k];
                         int nx = j + dx[k];
                         if(ny >= 0 && ny < R && nx >= 0 && nx < C) {
                             if(arr[ny][nx] != -1) {
@@ -67,14 +67,62 @@ public class Main {
     
     // 청정 top
     static void cleanTop() {
+        // 반대로 생각하자
+        // 시계방향으로 한칸씩 땡겨진다고 하자.
+        arr[top[0] - 1][top[1]] = 0;
+        for(int i = top[0] + 1; i > 0; i--) {
+            arr[i][top[1]] = arr[i - 1][top[1]];
+        }
+        // 최상단
+        for(int i = top[1]; i < C - 1; i++) {
+            arr[0][i] = arr[0][i + 1];
+        }
 
+        for (int i = 0; i < top[0] - 1; i++) {
+            arr[i][C - 1] = arr[i + 1][C - 1];
+        }
+
+        for(int i = C - 1; i < top[1] + 1; i--) {
+            arr[top[0]][i] = arr[top[0]][i - 1];
+        }
+
+        arr[top[0]][top[1] + 1] = 0;
     }
 
     // 청정 bottom
     static void cleanBot() {
+        // 반대로 생각하자
+        // 반시계방향으로 한칸씩 땡겨진다고 하자.
+        arr[bot[0] + 1][bot[1]] = 0;
+        for(int i = bot[0] + 1; i < R - 1; i++) {
+            arr[i][bot[1]] = arr[i + 1][bot[1]];
+        }
+        for(int i = bot[1]; i < C - 1; i++) {
+            arr[R - 1][i] = arr[R - 1][i + 1];
+        }
+        for(int i = R - 1; i > bot[0] + 1; i--) {
+            arr[i][C - 1] = arr[i - 1][C - 1];
+        }
+        for(int i = C - 1; i > bot[1] + 1; i--) {
+            arr[bot[0]][i] = arr[bot[0]][i - 1];
+        }
 
+        arr[bot[0]][bot[1] + 1] = 0;
     }
-    
+
+    // 남아있는 미세먼지 구하기
+    static int getTotal() {
+        int total = 0;
+        for(int i = 0; i < R; i++) {
+            for(int j = 0; j < C; j++) {
+                if(arr[i][j] > 0) {
+                    total += arr[i][j];
+                }
+            }
+        }
+        return total;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -99,7 +147,13 @@ public class Main {
         top = new int[]{temp[0] - 1, temp[1]};
         bot = new int[]{temp[0], temp[1]};
 
+        for(int i = 0; i < time; i++) {
+            spread();
+            cleanTop();
+            cleanBot();
+        }
 
+        System.out.println(getTotal());
 
         br.close();
     }
