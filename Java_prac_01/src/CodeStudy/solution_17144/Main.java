288ad416d4ff39d4beb0ed48cc35febed1a1b4ed
EunHyeok <secun77@naver.com>
2026-02-19 15:09:14 +0900

    17144 해결!

--- Java_prac_01/src/CodeStudy/solution_17144/Main.java
+++ Java_prac_01/src/CodeStudy/solution_17144/Main.java
@@ -34,10 +34,10 @@ public class Main {
                         }
                     }
                     // 확산될값
-                    int add = Math.ceilDiv(arr[i][j], count);
+                    int add = arr[i][j] / 5;
                     // 확산되고 남을 값
                     int remain = arr[i][j] - add * count;
-                    arr[i][j] = remain;
+                    temp[i][j] += remain;
                     // 새로 사용할 배열에 추가될 값을 저장
                     for(int k = 0; k < 4; k++) {
                         int ny = i + dy[k];
@@ -50,14 +50,8 @@ public class Main {
                     }
                     
                 }
-            }
-        }
-
-        // temp에 추가되는 값이 저장되고 나면 arr에 남아있는 remain들을 temp에 저장해야 함.
-        for(int i = 0; i < R; i++) {
-            for(int j = 0; j < C; j++) {
-                if(arr[i][j] > 0) {
-                    temp[i][j] += arr[i][j];
+                if(arr[i][j] == - 1) {
+                    temp[i][j] = -1;
                 }
             }
         }
@@ -67,47 +61,38 @@ public class Main {
     
     // 청정 top
     static void cleanTop() {
-        // 반대로 생각하자
-        // 시계방향으로 한칸씩 땡겨진다고 하자.
-        arr[top[0] - 1][top[1]] = 0;
-        for(int i = top[0] + 1; i > 0; i--) {
-            arr[i][top[1]] = arr[i - 1][top[1]];
+        for(int i = top[0] - 1; i > 0; i--) {
+            arr[i][0] = arr[i - 1][0];
         }
-        // 최상단
-        for(int i = top[1]; i < C - 1; i++) {
+        for(int i = 0; i < C - 1; i++) {
             arr[0][i] = arr[0][i + 1];
         }
-
-        for (int i = 0; i < top[0] - 1; i++) {
+        for(int i = 0; i < top[0]; i++) {
             arr[i][C - 1] = arr[i + 1][C - 1];
         }
-
-        for(int i = C - 1; i < top[1] + 1; i--) {
+        for(int i = C - 1; i > 1; i--) {
             arr[top[0]][i] = arr[top[0]][i - 1];
         }
+        arr[top[0]][1] = 0;
 
-        arr[top[0]][top[1] + 1] = 0;
     }
 
     // 청정 bottom
     static void cleanBot() {
-        // 반대로 생각하자
-        // 반시계방향으로 한칸씩 땡겨진다고 하자.
-        arr[bot[0] + 1][bot[1]] = 0;
         for(int i = bot[0] + 1; i < R - 1; i++) {
-            arr[i][bot[1]] = arr[i + 1][bot[1]];
+            arr[i][0] = arr[i + 1][0];
         }
-        for(int i = bot[1]; i < C - 1; i++) {
+        for(int i = 0; i < C - 1; i++) {
             arr[R - 1][i] = arr[R - 1][i + 1];
         }
-        for(int i = R - 1; i > bot[0] + 1; i--) {
+        for(int i = R - 1; i > bot[0]; i--) {
             arr[i][C - 1] = arr[i - 1][C - 1];
         }
-        for(int i = C - 1; i > bot[1] + 1; i--) {
+        for(int i = C - 1; i > 1; i--) {
             arr[bot[0]][i] = arr[bot[0]][i - 1];
         }
+        arr[bot[0]][1] = 0;
 
-        arr[bot[0]][bot[1] + 1] = 0;
     }
 
     // 남아있는 미세먼지 구하기
@@ -130,22 +115,24 @@ public class Main {
         C = Integer.parseInt(st.nextToken());
         time = Integer.parseInt(st.nextToken());
         arr = new int[R][C];
-        int[] temp = new int[2];
+        int idx = 0;
+        int[][] cleaner = new int[2][2];
         for(int i = 0; i < R; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             for(int j = 0; j < C; j++) {
                 int tp = Integer.parseInt(st.nextToken());
                 // 공기청정기 위치
                 if(tp == -1) {
-                    temp[0] = i;
-                    temp[1] = j;
+                    cleaner[idx][0] = i;
+                    cleaner[idx][1] = j;
+                    idx++;
                 }
                 arr[i][j] = tp;
             }
         }
         
-        top = new int[]{temp[0] - 1, temp[1]};
-        bot = new int[]{temp[0], temp[1]};
+        top = cleaner[0];
+        bot = cleaner[1];
 
         for(int i = 0; i < time; i++) {
             spread();
