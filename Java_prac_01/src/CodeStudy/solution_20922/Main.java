3d078379b6d8f7ce9337ba1925e506a729ad42b0
EunHyeok <secun77@naver.com>
2026-03-20 14:58:33 +0900

    20922 해결!

--- Java_prac_01/src/CodeStudy/solution_20922/Main.java
+++ Java_prac_01/src/CodeStudy/solution_20922/Main.java
@@ -3,8 +3,6 @@ package CodeStudy.solution_20922;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,8 +10,7 @@ public class Main {
     static int[] visited = new int[100001];
     static int[] arr;
     static int max;
-    static int result = 0;
-    static int count = 0;
+    static int result = 1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,39 +25,28 @@ public class Main {
 
         int left = 0;
         int right = left + 1;
-        visited[left] = 1;
+        visited[arr[left]] = 1;
 
         while(right < arr.length) {
-            // 겹치지 않는 경우
-            if(visited[arr[right]] == 0) {
-                visited[arr[right]]++;
-            }
-            // 겹치는 경우
-            else {
-                // 기존에 겹치던 얘들이랑 겹친 경우
-                // 문제 없음
-                if(visited[arr[right]] > 1) {
-                    visited[arr[right]]++;
-                }
-                // 새롭게 겹치는 얘들이 생긴 경우
-                else {
-                    visited[arr[right]]++;
-                    count++;
-                    // 개수 제한을 넘어가는 경우
-                    // left를 키워야 함.
-                    while(count > max && left <= right) {
-                        visited[arr[left]]--;
-                        if(visited[arr[left]] == 1) {
-                            count--;
-                        }
-                        left++;
-                    }
-                }
-            }
-            if(right - left + 1 > result) {
-                result = right - left + 1;
-            }
-            right++;
+           visited[arr[right]]++;
+           // right의 숫자가 max개 이상 겹친 경우
+           if(visited[arr[right]] > max) {
+               int tar = arr[right];
+               while(left <= right) {
+                   if(arr[left] == tar) {
+                       visited[arr[left]]--;
+                       left++;
+                       break;
+                   }
+                   visited[arr[left]]--;
+                   left++;
+               }
+           }
+           if(right - left + 1 > result) {
+               result = right - left + 1;
+           }
+
+           right++;
         }
 
         System.out.println(result);
